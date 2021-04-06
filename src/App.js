20285893d4d81500377227360930010d98c1d894
Oswaldo Antonio Sanchez <mago.ads@gmail.com>
2021-04-06 17:20:06 -0600

    fix map

--- src/App.js
+++ src/App.js
@@ -1,130 +1,164 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import "react-datepicker/dist/react-datepicker.css";
-import mails from './data'
-import logoImg from './assets/logo.png'
-import SysHead from './components/sysHead';
-import moment from 'moment'
-import MailContent from './components/showMail';
-
+import mails from "./data";
+import logoImg from "./assets/logo.png";
+import SysHead from "./components/sysHead";
+import moment from "moment";
+import MailContent from "./components/showMail";
 
 function App() {
-  const [allMails, setMails] = useState(mails)
-  const [refreshArray, setRefresh] = useState(false)
-  const [showBody, setBody] = useState(null)
-  const [theEmail, setEmail] = useState(null)
-  const [search, setSearch] = useState(null)
+  const [allMails, setMails] = useState(mails);
+  const [refreshArray, setRefresh] = useState(false);
+  const [showBody, setBody] = useState(null);
+  const [theEmail, setEmail] = useState(null);
+  const [search, setSearch] = useState(null);
 
   //update array on screen
   useEffect(() => {
-     setRefresh(true)
+    setRefresh(true);
   }, [refreshArray]);
-  
-  //getCurrentMails
-  const currentMails = allMails.length
 
-  function filterDate () {
-    console.log('filter')
+  //getCurrentMails
+  const currentMails = allMails.length;
+
+  function filterDate() {
+    console.log("filter");
   }
 
-  //search body of each mail 
+  //search body of each mail
   function handleInput(e) {
-     const params = e.target.value
-     setSearch(params)
-     let NewArray = []
+    const params = e.target.value;
+    setSearch(params);
+    let NewArray = [];
 
     if (params.length > 2) {
-     allMails.map((mail, i)=> {       
-        let str = mail.text;
+      for (let i = 0; i < allMails.length; i++) {
+        let str = allMails[i].text;
         let lowerCase = str.toLowerCase();
         let substr = search;
-        const exist =  lowerCase.includes(substr);
-        console.log(exist)
-        if(exist) {
-          NewArray = [...NewArray]  
-          console.log(NewArray)
-          NewArray.push(mail)
-           setMails(NewArray)
-          } else {
-            setMails(NewArray)            
-          }  
-       })
-      } else if (params < 2){
-        setMails(mails)
+        const exist = lowerCase.includes(substr);
+        console.log(exist);
+        if (exist) {
+          NewArray = [...NewArray];
+          console.log(NewArray);
+          NewArray.push(allMails[i]);
+          setMails(NewArray);
+        } else {
+          setMails(NewArray);
+        }
       }
+    } else if (params < 2) {
+      setMails(mails);
+    }
   }
-
 
   //getDate of the email
   function getDate(wholeDate) {
-   const newDate = moment(wholeDate).format('ll'); 
-    return newDate
+    const newDate = moment(wholeDate).format("ll");
+    return newDate;
   }
 
-
-  //remove an email 
+  //remove an email
   function removeEmail(i) {
-
-   let newArray = allMails 
-   newArray.splice(i, 1);
-   setMails(newArray)
-   setRefresh(false)
-
+    let newArray = allMails;
+    newArray.splice(i, 1);
+    setMails(newArray);
+    setRefresh(false);
   }
 
   //open the email body
   function openMail(i, mail) {
-    setEmail(mail)
-    console.log(mail)
-    setBody(true)
-   
+    setEmail(mail);
+    console.log(mail);
+    setBody(true);
   }
 
-  
   return (
     <div className="App">
-        <SysHead currentMails={currentMails} handleInput={handleInput}></SysHead>
+      <SysHead currentMails={currentMails} handleInput={handleInput}></SysHead>
 
-        <div className="mailBody" style={currentMails === 0 ? {backgroundImage: `url(${logoImg})`} : {}}>
-         {refreshArray ? 
-         <div>
-           {currentMails > 0 ? (
-             <div className={showBody ? 'gridOnmail' : 'notGrid'}>
-              <div className={showBody ? 'theTable mbHide' : 'theTable'}> 
-                 <div className="tableHeader">
-                     <div>From</div>
-                     <div style={showBody ? {display: 'none'} : {display: 'block'}}>To</div>
-                     <div>Subject</div>
-                     <div className="lastRow">Date <div className="filterBtn" onClick={filterDate}></div> </div>
-                 </div>
-                 <div className="tableBody">
-                    {allMails.map((mail, i)=>{
+      <div
+        className="mailBody"
+        style={currentMails === 0 ? { backgroundImage: `url(${logoImg})` } : {}}
+      >
+        {refreshArray ? (
+          <div>
+            {currentMails > 0 ? (
+              <div className={showBody ? "gridOnmail" : "notGrid"}>
+                <div className={showBody ? "theTable mbHide" : "theTable"}>
+                  <div className="tableHeader">
+                    <div>From</div>
+                    <div
+                      style={
+                        showBody ? { display: "none" } : { display: "block" }
+                      }
+                    >
+                      To
+                    </div>
+                    <div>Subject</div>
+                    <div className="lastRow">
+                      Date{" "}
+                      <div className="filterBtn" onClick={filterDate}></div>{" "}
+                    </div>
+                  </div>
+                  <div className="tableBody">
+                    {allMails.map((mail, i) => {
                       return (
-                         <div>
-                          <div className="eachMail" key={i}> 
-                            <div onClick={()=> openMail(i, mail)}>{showBody ? mail.from.substring(0, 8)+'...' : mail.from} </div>
-                            <div style={showBody ? {display: 'none'} : {display: 'block'}} onClick={()=> openMail(i, mail)}>{mail.recipient} </div>
-                            <div onClick={()=> openMail(i, mail)}>{mail.subject.substring(0, 35)}...
-                             {mail.attach ? <div className="haveAttach"></div> : '' }
+                        <div>
+                          <div className="eachMail" key={i}>
+                            <div onClick={() => openMail(i, mail)}>
+                              {showBody
+                                ? mail.from.substring(0, 8) + "..."
+                                : mail.from}{" "}
                             </div>
-                         
-                            <div className="theDate">{getDate(mail.Date)}
-                              <div className="removeMail" onClick={()=>removeEmail(i)}>Remove</div>
+                            <div
+                              style={
+                                showBody
+                                  ? { display: "none" }
+                                  : { display: "block" }
+                              }
+                              onClick={() => openMail(i, mail)}
+                            >
+                              {mail.recipient}{" "}
                             </div>
-                          </div>
-                  
-                          </div>
-                      )
-                    })} 
-                                      
-                 </div>
+                            <div onClick={() => openMail(i, mail)}>
+                              {mail.subject.substring(0, 35)}...
+                              {mail.attach ? (
+                                <div className="haveAttach"></div>
+                              ) : (
+                                ""
+                              )}
+                            </div>
 
+                            <div className="theDate">
+                              {getDate(mail.Date)}
+                              <div
+                                className="removeMail"
+                                onClick={() => removeEmail(i)}
+                              >
+                                Remove
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+                {showBody ? (
+                  <MailContent data={theEmail} showBody={setBody}></MailContent>
+                ) : (
+                  ""
+                )}{" "}
               </div>
-              {showBody ? <MailContent data={theEmail} showBody={setBody} ></MailContent> : ''} </div>
-
-           ) : ''}  
-           </div>  : '' }     
-        
-        </div>    
+            ) : (
+              ""
+            )}
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
     </div>
   );
 }
--- src/data.js
+++ src/data.js
@@ -1,53 +1,53 @@
 const mails = [
   {
-       "recipient":    "john@doe.com",
-        'Date': 'Thu, 4 Mar 2010 15:35:32 -0800',
-        "subject":      "This is a test mail",
-        "from":         "info@example.com",
-        "text":         "Hola este es un mensaje de prueba y es el primero 1...",
-        "html":         "<html><head></head><body>HELLO</body></html>"
+    recipient: "john@doe.com",
+    Date: "Thu, 4 Mar 2010 15:35:32 -0800",
+    subject: "This is a test mail",
+    from: "info@example.com",
+    text: "Hola este es un mensaje de prueba y es el primero 1...",
+    html: "<html><head></head><body>HELLO</body></html>",
   },
-   {
-    "recipient":    "bbbbbb@gmail.com",
-    'Date': 'Thu, 4 Mar 2010 15:35:32 -0800',
-     "subject":      "Web Contact",
-     "from":         "info@example.com",
-     "text":         "Second Test Message for testing the emails.",
-     "attach": true,
-     "html":         "<html><head></head><body>HELLO</body></html>"
-},
-{
-    "recipient":    "john@example.com",
-    'Date': 'Thu, 4 Mar 2010 15:35:32 -0800',
-     "subject":      "Notice of official announcement",
-     "from":         "info@example.com",
-     "text":         "third Option for filtering mail bodies",
-     "html":         "<html><head></head><body>HELLO</body></html>"
-},
-{
-    "recipient":    "john@doe.com",
-    'Date': 'Thu, 4 Mar 2010 15:35:32 -0800',
-     "subject":      "Happy New Year! Greetings for the New Year.",
-     "from":         "info@example.com",
-     "text":         "this is a Example and its the fourth example"
-},
-{
-     "recipient":    "john@doe.com",
-     'Date': 'Thu, 4 Mar 2010 15:35:32 -0800',
-      "subject":      "Happy New Year! Greetings for the New Year.",
-      "from":         "info@example.com",
-      "text":         "This is the messge body number 5...",
-      "html":         "<html><head></head><body>HELLO</body></html>"
- },
- {
-     "recipient":    "john@doe.com",
-     'Date': 'Thu, 4 Mar 2010 15:35:32 -0800',
-      "subject":      "Happy New Year! Greetings for the New Year.",
-      "from":         "info@example.com",
-      "attach": true,
-      "text":         "This is the messge body number 6...",
-      "html":         "<html><head></head><body>HELLO</body></html>"
- },
-]
+  {
+    recipient: "bbbbbb@gmail.com",
+    Date: "Thu, 4 Mar 2010 15:35:32 -0800",
+    subject: "Web Contact",
+    from: "info@example.com",
+    text: "Second Test Message for testing the emails.",
+    attach: true,
+    html: "<html><head></head><body>HELLO</body></html>",
+  },
+  {
+    recipient: "john@example.com",
+    Date: "Thu, 4 Mar 2010 15:35:32 -0800",
+    subject: "Notice of official announcement",
+    from: "info@example.com",
+    text: "third Option for filtering mail bodies",
+    html: "<html><head></head><body>HELLO</body></html>",
+  },
+  {
+    recipient: "john@doe.com",
+    Date: "Thu, 4 Mar 2010 15:35:32 -0800",
+    subject: "Happy New Year! Greetings for the New Year.",
+    from: "info@example.com",
+    text: "this is a Example and its the fourth example",
+  },
+  {
+    recipient: "john@doe.com",
+    Date: "Thu, 4 Mar 2010 15:35:32 -0800",
+    subject: "Happy New Year! Greetings for the New Year.",
+    from: "info@example.com",
+    text: "This is the messge body number 5...",
+    html: "<html><head></head><body>HELLO</body></html>",
+  },
+  {
+    recipient: "john@doe.com",
+    Date: "Thu, 4 Mar 2010 15:35:32 -0800",
+    subject: "Happy New Year! Greetings for the New Year.",
+    from: "info@example.com",
+    attach: true,
+    text: "This is the messge body number 6...",
+    html: "<html><head></head><body>HELLO</body></html>",
+  },
+];
 
-export default mails
+export default mails;
